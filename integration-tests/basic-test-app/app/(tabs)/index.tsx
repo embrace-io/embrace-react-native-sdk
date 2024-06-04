f98d897b9bc15d4eae929fdce5473af7e88a3551
Jonathan Munz <jpmunz@gmail.com>
2024-06-04 16:37:14 -0400

    EMBR-3940 integration test basic test app

---
+++ integration-tests/basic-test-app/app/(tabs)/index.tsx
@@ -0,0 +1,53 @@
+import { Image, StyleSheet, Button } from "react-native";
+import { useCallback } from "react";
+
+import { ThemedView } from "@/components/ThemedView";
+import { ThemedText } from "@/components/ThemedText";
+
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { endSession, logError } from "@embrace-io/react-native";
+
+const HomeScreen = () => {
+  const handleEndSession = useCallback(() => {
+    console.log("end session was clicked");
+    endSession();
+  }, []);
+
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
+      headerImage={
+        <Image
+          source={require("@/assets/images/partial-react-logo.png")}
+          style={styles.reactLogo}
+        />
+      }
+    >
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">End Session</ThemedText>
+        <Button onPress={handleEndSession} title="END SESSION" />
+      </ThemedView>
+    </ParallaxScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: "absolute",
+  },
+});
+
+export default HomeScreen;
