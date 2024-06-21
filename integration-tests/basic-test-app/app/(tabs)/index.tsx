1a83c9c0924600fe09232d6fba1139391915840c
Jonathan Munz <jpmunz@gmail.com>
2024-06-21 14:01:53 -0400

    reproduction steps for FD-4355

--- integration-tests/basic-test-app/app/(tabs)/index.tsx
+++ integration-tests/basic-test-app/app/(tabs)/index.tsx
@@ -5,13 +5,46 @@ import {ThemedView} from "@/components/ThemedView";
 import {ThemedText} from "@/components/ThemedText";
 
 import ParallaxScrollView from "@/components/ParallaxScrollView";
-import {endSession, logError} from "@embrace-io/react-native";
+import {endSession} from "@embrace-io/react-native";
+
+import { startSpan, stopSpan, addSpanEventToSpan } from "@embrace-io/react-native-spans";
 
 const HomeScreen = () => {
   const handleEndSession = useCallback(() => {
     console.log("end session was clicked");
     endSession();
   }, []);
+
+
+  const handleTestSpanAPI = useCallback(async () => {
+    try {
+     const spanId =  await startSpan("my-span");
+
+     if (typeof spanId === "boolean") {
+       console.error("Failed to start span");
+       return
+     }
+
+     /* fails on both iOS and Android, neither handle this being null
+     const addedEventNoTS = await addSpanEventToSpan(spanId, "my-span-event-no-ts");
+     if (!addedEventNoTS) {
+       console.error("Failed to add span event without TS");
+       return
+     }
+      */
+
+     const addedEventWithTS = await addSpanEventToSpan(spanId, "my-span-event-with-ts", new Date().getTime());
+     if (!addedEventWithTS) {
+       console.error("Failed to add span event with TS");
+       return
+     }
+
+     await stopSpan(spanId, "Failure")
+
+    } catch (error) {
+        console.error(`Failed to add event: ${error}`);
+    }
+  }, [])
 
   return (
     <ParallaxScrollView
@@ -25,6 +58,10 @@ const HomeScreen = () => {
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">End Session</ThemedText>
         <Button onPress={handleEndSession} title="END SESSION" />
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Performance Tracing</ThemedText>
+        <Button onPress={handleTestSpanAPI} title="TEST SPAN API" />
       </ThemedView>
     </ParallaxScrollView>
   );
