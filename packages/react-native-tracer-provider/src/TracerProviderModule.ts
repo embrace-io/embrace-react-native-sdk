eb883b8e7a999688dbed602da5dbbff89d7f10ba
Jonathan Munz <jpmunz@gmail.com>
2024-07-08 16:27:03 -0400

    EBMR-4247 JS layer for tracer provider

---
+++ packages/react-native-tracer-provider/src/TracerProviderModule.ts
@@ -0,0 +1,18 @@
+import { NativeModules, Platform  } from 'react-native';
+
+const LINKING_ERROR =
+  `The package 'react-native-tracer-provider' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: '- You have run \'pod install\'\n', default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo Go\n';
+
+export const TracerProviderModule = NativeModules.TracerProviderModule
+  ? NativeModules.TracerProviderModule
+  : new Proxy(
+      {},
+      {
+        get() {
+          throw new Error(LINKING_ERROR);
+        },
+      }
+    );
