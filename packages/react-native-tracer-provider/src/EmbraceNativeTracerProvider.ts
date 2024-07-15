bcd5fc8a29dff99215dbdcdfc1166609537f3ccc
Jonathan Munz <jpmunz@gmail.com>
2024-07-15 10:17:57 -0400

    EMBR-4290 update JS tracer provider with extra guards (#51)

--- packages/react-native-tracer-provider/src/EmbraceNativeTracerProvider.ts
+++ packages/react-native-tracer-provider/src/EmbraceNativeTracerProvider.ts
@@ -1,3 +1,5 @@
+import {NativeModules} from "react-native";
+import {useEffect, useState} from "react";
 import {
   context,
   ContextManager,
@@ -5,13 +7,79 @@ import {
   TracerProvider,
 } from "@opentelemetry/api";
 
+import {logWarning} from "./util";
 import {
   EmbraceNativeTracerProviderConfig,
   SpanContextSyncBehaviour,
+  UseEmbraceNativeTracerProviderResult,
 } from "./types";
 import {TracerProviderModule} from "./TracerProviderModule";
 import {StackContextManager} from "./StackContextManager";
 import {EmbraceNativeTracer} from "./EmbraceNativeTracer";
+
+/**
+ * useEmbraceNativeTracerProvider makes sure that the Embrace SDK has been installed and started and
+ * then sets up a EmbraceNativeTracerProvider
+ *
+ * The EmbraceNativeTracerProvider implements a TracerProvider over the native Embrace Android and iOS SDKs.
+ * Thin wrapped objects representing Tracers and Spans are maintained at the JS level and use Native Modules to
+ * call down to the SDKs to perform the actual operations on them.
+ *
+ * The JS side of its implementation is modelled after [opentelemetry-sdk-trace-base](https://github.com/open-telemetry/opentelemetry-js/tree/main/packages/opentelemetry-sdk-trace-base)
+ */
+export const useEmbraceNativeTracerProvider = (
+  config?: EmbraceNativeTracerProviderConfig,
+): UseEmbraceNativeTracerProviderResult => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isError, setIsError] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
+  const [tracerProvider, setTracerProvider] = useState<TracerProvider | null>(
+    null,
+  );
+
+  useEffect(() => {
+    if (error) {
+      logWarning(error);
+    }
+  }, [error]);
+
+  useEffect(() => {
+    if (!NativeModules.EmbraceManager) {
+      setError(
+        "You must have the Embrace SDK available to use the TracerProvider, please install `@embrace-io/react-native`.",
+      );
+      setIsError(true);
+      setIsLoading(false);
+      return;
+    }
+
+    if (isLoading) {
+      NativeModules.EmbraceManager.isStarted()
+        .then((started: boolean) => {
+          if (started) {
+            setTracerProvider(new EmbraceNativeTracerProvider(config));
+          } else {
+            setError(
+              "The Embrace SDK must be started to use the TracerProvider, please invoke `initialize` from `@embrace-io/react-native`.",
+            );
+            setIsError(true);
+          }
+        })
+        .catch(() => {
+          setError("Failed to setup EmbraceNativeTracerProvider");
+          setIsError(true);
+        })
+        .finally(() => setIsLoading(false));
+    }
+  }, [isLoading, config]);
+
+  return {
+    isLoading,
+    isError,
+    error,
+    tracerProvider,
+  };
+};
 
 /**
  * EmbraceNativeTracerProvider implements a TracerProvider over the native Embrace Android and iOS SDKs.
@@ -20,7 +88,7 @@ import {EmbraceNativeTracer} from "./EmbraceNativeTracer";
  *
  * The JS side of this implementation is modelled after [opentelemetry-sdk-trace-base](https://github.com/open-telemetry/opentelemetry-js/tree/main/packages/opentelemetry-sdk-trace-base)
  */
-export class EmbraceNativeTracerProvider implements TracerProvider {
+class EmbraceNativeTracerProvider implements TracerProvider {
   private readonly contextManager: ContextManager;
   private readonly spanContextSyncBehaviour: SpanContextSyncBehaviour;
 
@@ -36,7 +104,8 @@ export class EmbraceNativeTracerProvider implements TracerProvider {
       context.setGlobalContextManager(this.contextManager);
     }
 
-    this.spanContextSyncBehaviour = config.spanContextSyncBehaviour || "block";
+    this.spanContextSyncBehaviour =
+      config.spanContextSyncBehaviour || "return_empty";
   }
 
   public getTracer(
