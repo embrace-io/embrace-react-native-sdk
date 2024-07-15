795faefd8534891c027ecbeb3213bb6107cab853
Jonathan Munz <jpmunz@gmail.com>
2024-07-15 11:04:25 -0400

    backport changes from EMBR-4253

---
+++ packages/react-native-tracer-provider/update_android_version.ts
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+const packageJSON = require("./package.json");
+
+const gradleFile = "native-src/android/app/dependencies.gradle";
+
+const contents = fs.readFileSync(gradleFile).toString();
+const updated = contents.replace(
+  /(io.embrace:embrace-android-sdk:)\d+.\d+.\d+/,
+  `$1${packageJSON.embrace.androidVersion}`,
+);
+
+fs.writeFileSync(gradleFile, updated);
