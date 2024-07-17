ead0db32ebc81fdad75b2b1bb085ae430658ada0
Jonathan Munz <jpmunz@gmail.com>
2024-07-17 18:05:12 -0400

    EMBR-4343 add actual test

--- packages/core/test-project/ios/RNEmbraceTests/RNEmbraceTests.swift
+++ packages/core/test-project/ios/RNEmbraceTests/RNEmbraceTests.swift
@@ -2,15 +2,35 @@ import XCTest
 import EmbraceIO
 @testable import RNEmbrace
 
+final class Promise {
+  var resolveCalls: [Bool] = []
+  
+  func resolve(val: Any?) -> Void {
+    if let val = val as? Bool {
+      resolveCalls.append(val)
+    }
+  }
+  
+  func reject(a: String?, b: String?, c: Error?) -> Void {}
+}
+
 
 final class EmbraceManagerTests: XCTestCase {
   var module: EmbraceManager!;
+  var promise: Promise!;
 
   override func setUp() {
-    module = EmbraceManager();
+      promise = Promise()
+      module = EmbraceManager();
   }
 
-  func testBasic()  {
-    XCTAssertEqual(1, 1)
+  func testStartNativeEmbraceSDK()  {
+      module.startNativeEmbraceSDK("myApp", resolve: promise.resolve, rejecter: promise.reject)
+      XCTAssertEqual(promise.resolveCalls.count, 1)
+      XCTAssertTrue(promise.resolveCalls[0])
+      
+      module.isStarted(promise.resolve, rejecter: promise.reject)
+      XCTAssertEqual(promise.resolveCalls.count, 2)
+      XCTAssertTrue(promise.resolveCalls[1])
   }
 }
