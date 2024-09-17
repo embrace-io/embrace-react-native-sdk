0296ce1b71ef6a431a8b11816e0b8e8dc20eea00
Florencia Acosta <florencia.acosta@embrace.io>
2024-09-17 11:24:09 -0300

    ios setting

--- packages/react-native-otlp/ios/RNEmbraceOTLP/RNEmbraceOTLP.swift
+++ packages/react-native-otlp/ios/RNEmbraceOTLP/RNEmbraceOTLP.swift
@@ -1,22 +1,49 @@
 import React
 import Foundation
 import EmbraceIO
+import EmbraceCommonInternal
 
 @objc(RNEmbraceOTLP)
 class RNEmbraceOTLP: NSObject {
-    @objc(setCustomExporter:header:token:resolver:rejecter:)
-    func setCustomExporter (_ endpoint: String,
-                            header: String,
-                            token: String,
-                            resolver resolve: RCTPromiseResolveBlock,
-                            rejecter reject: RCTPromiseRejectBlock) {
-        
-
-        resolve(true)
-    }
-  
-  @objc
-  func setCustomOtlpGrpcSpanExporter() {
+  @objc(setCustomExporter:header:token:resolver:rejecter:)
+  func setCustomExporter (_ endpoint: String,
+                          header: String,
+                          token: String,
+                          resolver resolve: RCTPromiseResolveBlock,
+                          rejecter reject: RCTPromiseRejectBlock) {
     
+    if (token.isEmpty) {
+      reject("INIT_CUSTOM_EXPORTER_ERROR", "Token can't be empty", nil)
+    }
+    
+    let urlConfig = URLSessionConfiguration.default
+    urlConfig.httpAdditionalHeaders = ["Authorization": "Basic \(token)"]
+    
+//    try? Embrace
+//      .setup(
+//        options: Embrace.Options(
+//          appId: "AppID",
+//          logLevel: .debug,
+//          export: OpenTelemetryExport(
+//            spanExporter: OtlpHttpTraceExporter(
+//              endpoint: URL(string: "https://otlp-gateway-prod-us-west-0.grafana.net/otlp/v1/traces")!,
+//              useSession: URLSession(configuration: urlConfig)
+//            ),
+//            logExporter: OtlpHttpLogExporter(
+//              endpoint: URL(string: "https://otlp-gateway-prod-us-west-0.grafana.net/otlp/v1/logs")!,
+//              useSession: URLSession(configuration: urlConfig)
+//            )
+//          )
+//        )
+//      )
+//      .start()
+    
+    
+    resolve(true)
+  }
+
+  @objc
+  func initCustomExporter() {
+
   }
 }
