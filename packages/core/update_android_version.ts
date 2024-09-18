e4d41882af255e55bfccb878c56c1d510aa3ebf4
Jonathan Munz <jpmunz@gmail.com>
2024-09-18 17:11:51 -0400

    fix yarn build

--- packages/core/update_android_version.ts
+++ packages/core/update_android_version.ts
@@ -1,10 +1,10 @@
-export {};
+{
+  const fs = require("fs");
 
-const fs = require("fs");
+  const packageJSON = require("./package.json");
 
-const packageJSON = require("./package.json");
-
-fs.writeFileSync(
-  "android/gradle.properties",
-  `emb_android_sdk=${packageJSON.embrace.androidVersion}`,
-);
+  fs.writeFileSync(
+    "android/gradle.properties",
+    `emb_android_sdk=${packageJSON.embrace.androidVersion}`,
+  );
+}
