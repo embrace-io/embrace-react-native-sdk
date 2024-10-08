344ae5dff351689bf7590453129171ba19770b1f
Jonathan Munz <jpmunz@gmail.com>
2024-10-08 15:20:59 -0400

    EMBR-5159 integration test harness package

--- integration-tests/basic-test-app/app/_layout.tsx
+++ integration-tests/basic-test-app/app/_layout.tsx
@@ -1,33 +1,13 @@
-import {DarkTheme, DefaultTheme, ThemeProvider} from "@react-navigation/native";
 import {useFonts} from "expo-font";
-import {Stack} from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import {useEffect} from "react";
+import React, {useEffect} from "react";
 import "react-native-reanimated";
-import {initialize as initEmbrace} from "@embrace-io/react-native";
-
-import {useColorScheme} from "@/hooks/useColorScheme";
+import {EmbraceTestHarness} from "@embrace-io/react-native-test-harness";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  useEffect(() => {
-    const init = async () => {
-      await initEmbrace({
-        sdkConfig: {
-          ios: {
-            appId: "abcdf",
-            // endpointBaseUrl: "http://localhost:8877",
-          },
-        },
-      });
-    };
-
-    init();
-  }, []);
-
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
@@ -43,11 +23,13 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{headerShown: false}} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <EmbraceTestHarness
+      sdkConfig={{
+        ios: {
+          appId: "abcdf",
+          endpointBaseUrl: "http://localhost:8877",
+        },
+      }}
+    />
   );
 }
