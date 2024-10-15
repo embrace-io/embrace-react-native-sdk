0f2ab5a406448b646a2c8a4c81eea68fdb1fc7fb
Florencia Acosta <florencia.acosta@embrace.io>
2024-10-15 11:52:49 -0300

    [EMBR-4352] Run Swiftlint/Clang-format over RN iOS module

---
+++ scripts/run-swiftlint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+FORMATTER="swiftlint"
+
+# install if the formatter is not found
+if ! command -v ${FORMATTER} &> /dev/null; then
+    echo "${FORMATTER} not found. Proceeding to install."  
+
+    brew install swiftlint
+
+    export PATH="/opt/homebrew/opt/swiftlint/bin:$PATH"
+fi
+
+swiftlint --fix
