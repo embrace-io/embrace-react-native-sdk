2ca50fdbd1b71379ba118d00498152d231ba3497
Jonathan Munz <jpmunz@gmail.com>
2024-10-23 15:13:38 -0400

    EMBR-5083 remove unneeded files

--- integration-tests/basic-test-app/app/_layout.tsx
+++ integration-tests/basic-test-app/app/_layout.tsx
@@ -1,38 +1,8 @@
-import {useFonts} from "expo-font";
-import * as SplashScreen from "expo-splash-screen";
-import React, {useEffect} from "react";
+import React from "react";
 import "react-native-reanimated";
 import {EmbraceTestHarness} from "@embrace-io/react-native-test-harness";
-import {Stack} from "expo-router";
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+import sdkConfig from "./embrace-sdk-config.json";
 
 export default function RootLayout() {
-  const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
-  return (
-    <EmbraceTestHarness
-      navigationStyle="expo"
-      sdkConfig={{
-        ios: {
-          appId: "abcdf",
-          endpointBaseUrl: "http://localhost:8877",
-          disableAutomaticViewCapture: true,
-        },
-      }}
-    />
-  );
+  return <EmbraceTestHarness navigationStyle="expo" sdkConfig={sdkConfig} />;
 }
