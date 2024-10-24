51a309d48f7b35fdd65c986ffd3f676cf25724d6
Florencia Acosta <florencia.acosta@embrace.io>
2024-10-24 18:14:27 -0300

    removing ignored files

---
+++ packages/core/src/interfaces/common/index.ts
@@ -0,0 +1,32 @@
+/**
+ * Defines the SDK configuration that can be passed into `initialize`
+ */
+
+interface IOSConfig {
+  appId: string;
+  appGroupId?: string;
+  disableCrashReporter?: boolean;
+  disableAutomaticViewCapture?: boolean;
+  endpointBaseUrl?: string;
+}
+
+// NOTE: as per today Android is not configurable through code, this is a placeholder for future implementations.
+// Not even sure if it will ever happen
+interface AndroidConfig {}
+
+interface IOSConfig {
+  appId: string;
+  appGroupId?: string;
+  disableCrashReporter?: boolean;
+  disableAutomaticViewCapture?: boolean;
+  endpointBaseUrl?: string;
+}
+
+interface SDKConfig {
+  ios?: IOSConfig;
+  startCustomExport?: (
+    config: IOSConfig | NonNullable<object>,
+  ) => Promise<boolean>;
+}
+
+export {SDKConfig, IOSConfig, AndroidConfig};
