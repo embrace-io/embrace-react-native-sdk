8524e6e6e8bf1fd2e49e344b63213cd2f807a296
Florencia Acosta <florencia.acosta@embrace.io>
2024-11-04 15:37:33 -0300

    [EMBR-5552] Integrate custom export config for Traces and Logs

--- packages/react-native-otlp/android/src/main/java/io/embrace/rnembraceotlp/RNEmbraceOTLPModule.kt
+++ packages/react-native-otlp/android/src/main/java/io/embrace/rnembraceotlp/RNEmbraceOTLPModule.kt
@@ -1,50 +1,144 @@
 package io.embrace.rnembraceotlp
 
-import android.util.Log
+import java.time.Duration
+import java.util.logging.Logger
+import kotlin.time.DurationUnit
+import kotlin.time.toDuration
+import kotlin.time.toJavaDuration
+
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.ReadableArray
 import com.facebook.react.bridge.ReadableMap
 
 import io.embrace.android.embracesdk.Embrace
+import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporter
+import io.opentelemetry.sdk.trace.export.SpanExporter
+
+data class HeaderConfig (
+    val key: String,
+    val token: String
+)
+
+data class ExporterConfig(
+    val endpoint: String,
+    val headers: List<HeaderConfig>? = emptyList(),
+    val timeout: Duration? = null
+)
+
+data class OtlpExporterConfig (
+    val traceExporter: ExporterConfig? = null,
+    val logExporter: ExporterConfig? = null
+)
+
+// parsing Readable Map into the real OTLP Exporter Config shape
+fun parseExportConfig(exportConfig: ReadableMap): OtlpExporterConfig {
+    var spanExportConfigParsed: ExporterConfig? = null
+    val spanExportConfig = exportConfig.getMap("traceExporter")
+    val spanExportEndpoint = spanExportConfig?.getString("endpoint")
+
+    if (spanExportConfig != null && !spanExportEndpoint.isNullOrBlank()) {
+        val headersExportConfig = spanExportConfig.getArray("headers")?.let {
+            parseHeaders(it)
+        }
+
+        val timeoutExportConfig = parseTimeout(spanExportConfig.getDouble("timeout"))
+
+        spanExportConfigParsed = ExporterConfig(
+            endpoint = spanExportEndpoint,
+            headers = headersExportConfig,
+            timeout = timeoutExportConfig,
+        )
+    }
+
+    return OtlpExporterConfig(traceExporter = spanExportConfigParsed)
+}
+
+fun parseHeaders(headers: ReadableArray): List<HeaderConfig> {
+    val headerList: MutableList<HeaderConfig> = arrayListOf()
+
+    for (i in 0 until headers.size()) {
+        val header = headers.getMap(i)
+        val keyVal = header.getString("key")
+        val tokenVal = header.getString("token")
+
+        if (!keyVal.isNullOrBlank() && !tokenVal.isNullOrBlank()) {
+            headerList.add(HeaderConfig(key = keyVal, token = tokenVal))
+        }
+    }
+
+    return headerList
+}
+
+fun parseTimeout(number: Double): Duration {
+    return number.toLong().toDuration(DurationUnit.SECONDS).toJavaDuration()
+}
 
 class RNEmbraceOTLPModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
     override fun getName() = "RNEmbraceOTLP"
+
     private val context: ReactApplicationContext = reactContext
+    private val log = Logger.getLogger("[Embrace]")
 
-    @ReactMethod fun setOtlpHttpTraceExporter() {
+    @ReactMethod
+    fun setOtlpHttpTraceExporter(endpoint: String, headers: List<HeaderConfig>? = emptyList(), timeout: Duration?): SpanExporter {
+        val otlpHttpSpanExporter = OtlpHttpSpanExporter.builder()
+            .setEndpoint(endpoint)
+
+        if (!headers.isNullOrEmpty()) {
+            for (header in headers) {
+                otlpHttpSpanExporter.addHeader(header.key, header.token)
+            }
+        }
+
+        if (timeout != null) {
+            otlpHttpSpanExporter.setTimeout(timeout)
+        }
+
+        return otlpHttpSpanExporter.build()
+    }
+
+    @ReactMethod
+    fun setOtlpHttpLogExporter() {
         // placeholder
     }
 
-    @ReactMethod fun setOtlpHttpLogExporter() {
-        // placeholder
-    }
+    @ReactMethod
+    fun setHttpExporters(spanConfig: ExporterConfig? = null, logConfig: ExporterConfig? = null) {
+        if (spanConfig != null && spanConfig.endpoint.isNotBlank()) {
+            val spanCustomExporter = setOtlpHttpTraceExporter(spanConfig.endpoint, spanConfig.headers, spanConfig.timeout)
+            Embrace.getInstance().addSpanExporter(spanCustomExporter)
+        }
 
-    @ReactMethod fun setHttpExporters() {
-        // placeholder
+        if (logConfig != null) {
+            // setOtlpHttpLogExporter()
+            // tbd logs
+        }
     }
 
     // _sdkConfig is meant to not be used
-    @ReactMethod fun startNativeEmbraceSDK(_sdkConfig: ReadableMap, otlpExporterConfig: ReadableMap, promise: Promise) {
+    @ReactMethod fun startNativeEmbraceSDK(sdkConfig: ReadableMap, otlpExporterConfig: ReadableMap? = null, promise: Promise) {
         try {
-            // 1) Initialize custom export
-            // TBD
+            // 1) Initialize custom export if there is config
+            if (otlpExporterConfig != null) {
+                val otlpExportConfig = parseExportConfig(otlpExporterConfig)
+                setHttpExporters(otlpExportConfig.traceExporter, otlpExportConfig.logExporter)
+            }
 
-            // 2)
-            // Embrace Start
+            // 2) Embrace Start
             Embrace.getInstance().start(
                 this.context.getApplicationContext(),
                 false,
                 Embrace.AppFramework.REACT_NATIVE
             )
 
-            Log.i("Embrace", "RNEmbraceOTLP starting and working fine from Native side")
-
+            log.info("RNEmbraceOTLP starting and working fine from Native side")
             promise.resolve(true)
         } catch (e: Exception) {
             // TBD improve or remove
-            Log.i("Embrace", "Issues with Initialization")
+            log.info("Issues with Initialization")
             promise.resolve(false)
         }
     }
