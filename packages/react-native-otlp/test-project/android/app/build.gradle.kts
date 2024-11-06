65106b6c7b15be6ece0a9c29c8a3f386ea01d02c
Florencia Acosta <florencia.acosta@embrace.io>
2024-11-06 16:29:45 -0300

    [EMBR-5419] Integrate native test flow for Custom Export Config

---
+++ packages/react-native-otlp/test-project/android/app/build.gradle.kts
@@ -0,0 +1,73 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+
+    id("io.gitlab.arturbosch.detekt") version("1.23.6")
+}
+
+android {
+    namespace = "io.embrace.rnembraceotlptest"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "io.embrace.rnembraceotlptest"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    useLibrary("android.test.mock")
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    buildToolsVersion = "33.0.0"
+    ndkVersion = "20.1.5948944"
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+
+    // react native 0.75.x
+    implementation("com.facebook.react:react-android:+")
+
+    // TBD: use local package
+     implementation(project(":react-native-otlp"))
+
+    testImplementation(libs.junit)
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.3.1")
+    testImplementation("io.mockk:mockk:1.13.11")
+
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    add("detektPlugins", "io.gitlab.arturbosch.detekt:detekt-formatting:1.23.6")
+}
+
+detekt {
+    buildUponDefaultConfig = true
+    autoCorrect = true
+    config.from(project.files("${project.rootDir}/config/detekt/detekt.yml"))
+}
+
+apply("../../../android/dependencies.gradle")
---
+++ packages/react-native-otlp/test-project/copy-native-src.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# See DEVELOPING.md
+
+# Android
+# cp -r ../android/src/main/java android/app/src/main
+
+# iOS, not needed since the project in XCode can reference files from other locations
