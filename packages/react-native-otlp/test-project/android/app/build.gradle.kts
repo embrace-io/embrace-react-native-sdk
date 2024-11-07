b7ef6f82cf795995b6af2e2f338ad378d6037f52
Florencia Acosta <florencia.acosta@embrace.io>
2024-11-07 09:21:58 -0300

    wip

--- packages/react-native-otlp/test-project/android/app/build.gradle.kts
+++ packages/react-native-otlp/test-project/android/app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
 
-    id("io.gitlab.arturbosch.detekt") version("1.23.6")
+//    id("io.gitlab.arturbosch.detekt") version("1.23.6")
 }
 
 android {
@@ -29,10 +29,24 @@ android {
         }
     }
 
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
     useLibrary("android.test.mock")
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 
     buildToolsVersion = "33.0.0"
-    ndkVersion = "20.1.5948944"
+    ndkVersion = "26.1.10909125"
+
+    defaultConfig {
+        ndk {
+            abiFilters += listOf("armeabi-v7a", "x86", "x86_64")
+        }
+    }
 }
 
 dependencies {
@@ -40,26 +54,34 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
 
+    // fixing java.lang.UnsatisfiedLinkError during tests
+    implementation("com.facebook.fresco:fresco:2.0.0")
     // react native 0.75.x
     implementation("com.facebook.react:react-android:+")
 
-    // TBD: use local package
-     implementation(project(":react-native-otlp"))
+    // using package locally
+    implementation(project(":react-native-otlp"))
+
+    implementation("org.webkit:android-jsc:+")
 
     testImplementation(libs.junit)
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testImplementation("junit:junit:4.12")
     testImplementation("org.mockito.kotlin:mockito-kotlin:5.3.1")
     testImplementation("io.mockk:mockk:1.13.11")
+
+    testImplementation("org.robolectric:robolectric:4.8")
 
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    add("detektPlugins", "io.gitlab.arturbosch.detekt:detekt-formatting:1.23.6")
+//    add("detektPlugins", "io.gitlab.arturbosch.detekt:detekt-formatting:1.23.6")
 }
 
-detekt {
-    buildUponDefaultConfig = true
-    autoCorrect = true
-    config.from(project.files("${project.rootDir}/config/detekt/detekt.yml"))
-}
+//detekt {
+//    buildUponDefaultConfig = true
+//    autoCorrect = true
+//    config.from(project.files("${project.rootDir}/config/detekt/detekt.yml"))
+//}
 
 apply("../../../android/dependencies.gradle")
--- packages/react-native-otlp/test-project/android/app/src/test/java/io/embrace/rnembraceotlptest/RNEmbraceOTLPTest.kt
+++ packages/react-native-otlp/test-project/android/app/src/test/java/io/embrace/rnembraceotlptest/RNEmbraceOTLPTest.kt
@@ -1,28 +1,42 @@
 package io.embrace.rnembraceotlptest
 
+import android.os.SystemClock
+import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReadableMap
 import io.embrace.rnembraceotlp.RNEmbraceOTLPModule
+import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
-import org.mockito.kotlin.any
-import org.mockito.kotlin.argumentCaptor
-import org.mockito.kotlin.clearInvocations
-import org.mockito.kotlin.doReturn
+import org.mockito.Mockito
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.times
+import com.facebook.react.bridge.JavaOnlyMap
+import com.facebook.react.bridge.WritableMap
+import com.facebook.react.bridge.WritableNativeMap
 import org.mockito.kotlin.verify
 
-
-import org.junit.Assert.*
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class RNEmbraceOTLPTest {
+    val promise: Promise = mock()
+
+    @Before
+    fun setUp() {
+        // Mocking SystemClock.uptimeMillis() using Mockito
+        Mockito.mockStatic(SystemClock::class.java)
+        Mockito.`when`(SystemClock.uptimeMillis()).thenReturn(123456789L)
+    }
+
     @Test
-    fun addition_isCorrect() {
+    fun testStartNativeEmbraceSDK() {
         val context: ReactApplicationContext = mock()
-        val otlpModule = RNEmbraceOTLPModule(context);
+        val embraceOTLPModule = RNEmbraceOTLPModule(context)
+
+        val sdkConfig: ReadableMap = WritableNativeMap()
+        val otlpConfig: ReadableMap = JavaOnlyMap()
+
+        embraceOTLPModule.startNativeEmbraceSDK(sdkConfig, otlpConfig, promise)
+
+        // embrace starts
+        verify(promise, times(1)).resolve(true)
     }
 }
