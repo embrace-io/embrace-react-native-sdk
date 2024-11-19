079de9dda61f571e6a7c0037407e822054b905b3
Florencia Acosta <florencia.acosta@embrace.io>
2024-11-19 10:57:42 -0300

    refactor for otlp

--- packages/react-native-otlp/test-project/android/app/build.gradle.kts
+++ packages/react-native-otlp/test-project/android/app/build.gradle.kts
@@ -1,47 +1,109 @@
 plugins {
+    id("com.facebook.react")
+
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-
-    id("io.gitlab.arturbosch.detekt") version("1.23.6")
+    id("io.gitlab.arturbosch.detekt") version "1.23.6"
 }
 
+/**
+ * This is the configuration block to customize your React Native Android app.
+ * By default you don't need to apply any configuration, just uncomment the lines you need.
+ */
+react {
+    /* Folders */
+    //   The root of your project, i.e. where "package.json" lives. Default is '../..'
+    // root = file("../../")
+    //   The folder where the react-native NPM package is. Default is ../../node_modules/react-native
+    // reactNativeDir = file("../../node_modules/react-native")
+    //   The folder where the react-native Codegen package is. Default is ../../node_modules/@react-native/codegen
+    // codegenDir = file("../../node_modules/@react-native/codegen")
+    //   The cli.js file which is the React Native CLI entrypoint. Default is ../../node_modules/react-native/cli.js
+    // cliFile = file("../../node_modules/react-native/cli.js")
+
+    /* Variants */
+    //   The list of variants to that are debuggable. For those we're going to
+    //   skip the bundling of the JS bundle and the assets. By default is just 'debug'.
+    //   If you add flavors like lite, prod, etc. you'll have to list your debuggableVariants.
+    // debuggableVariants = ["liteDebug", "prodDebug"]
+
+    /* Bundling */
+    //   A list containing the node command and its flags. Default is just 'node'.
+    // nodeExecutableAndArgs = ["node"]
+    //
+    //   The command to run when bundling. By default is 'bundle'
+    // bundleCommand = "ram-bundle"
+    //
+    //   The path to the CLI configuration file. Default is empty.
+    // bundleConfig = file(../rn-cli.config.js)
+    //
+    //   The name of the generated asset file containing your JS bundle
+    // bundleAssetName = "MyApplication.android.bundle"
+    //
+    //   The entry file for bundle generation. Default is 'index.android.js' or 'index.js'
+    // entryFile = file("../js/MyApplication.android.js")
+    //
+    //   A list of extra flags to pass to the 'bundle' commands.
+    //   See https://github.com/react-native-community/cli/blob/main/docs/commands.md#bundle
+    // extraPackagerArgs = []
+
+    /* Hermes Commands */
+    //   The hermes compiler command to run. By default it is 'hermesc'
+    // hermesCommand = "$rootDir/my-custom-hermesc/bin/hermesc"
+    //
+    //   The list of flags to pass to the Hermes compiler. By default is "-O", "-output-source-map"
+    // hermesFlags = ["-O", "-output-source-map"]
+
+    /* Autolinking */
+    autolinkLibrariesWithApp()
+}
+
+/**
+ * Set this to true to Run Proguard on Release builds to minify the Java bytecode.
+ */
+val enableProguardInReleaseBuilds = false
+
 android {
-    namespace = "io.embrace.rnembraceotlptest"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "io.embrace.rnembraceotlptest"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
+    ndkVersion = rootProject.extra["ndkVersion"] as String
+    buildToolsVersion = rootProject.ext["buildToolsVersion"] as String
+    compileSdk = rootProject.ext["compileSdkVersion"] as Int
 
     buildTypes {
-        release {
+        getByName("debug") {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+        getByName("release") {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            signingConfig = signingConfigs.getByName("debug")
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
         }
     }
 
+    namespace = "io.embrace.rnembraceotlptest"
+    defaultConfig {
+        applicationId = "io.embrace.rnembraceotlptest"
+        minSdk = rootProject.ext["minSdkVersion"] as Int
+        targetSdk = rootProject.ext["targetSdkVersion"] as Int
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
     useLibrary("android.test.mock")
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
-
-    buildToolsVersion = "33.0.0"
-    ndkVersion = "26.1.10909125"
 }
+
+/**
+ * The preferred build flavor of JavaScriptCore (JSC)
+ *
+ * For example, to use the international variant, you can use:
+ * `def jscFlavor = 'org.webkit:android-jsc-intl:+'`
+ *
+ * The international variant includes ICU i18n library and necessary data
+ * allowing to use e.g. `Date.toLocaleString` and `String.localeCompare` that
+ * give correct results when using with locales other than en-US. Note that
+ * this variant is about 6MiB larger per architecture than default.
+ */
+val jscFlavor = "org.webkit:android-jsc:+"
 
 dependencies {
     implementation(libs.androidx.core.ktx)
@@ -50,10 +112,9 @@ dependencies {
 
     // using package locally
     implementation(project(":react-native-otlp"))
+    implementation("com.facebook.react:react-android")
 
     testImplementation(libs.junit)
-
-    testImplementation("com.facebook.react:react-android:0.75.1")
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     testImplementation("junit:junit:4.12")
@@ -67,6 +128,13 @@ dependencies {
 
     add("detektPlugins", "io.gitlab.arturbosch.detekt:detekt-formatting:1.23.6")
     implementation(kotlin("script-runtime"))
+
+    val isHermes = project.properties["hermesEnabled"];
+    if (isHermes.toString().toBoolean()) {
+        implementation("com.facebook.react:hermes-android")
+    } else {
+        implementation(jscFlavor)
+    }
 }
 
 detekt {
--- packages/react-native-otlp/test-project/android/build.gradle.kts
+++ packages/react-native-otlp/test-project/android/build.gradle.kts
@@ -3,3 +3,26 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
 }
+
+buildscript {
+    extra.apply {
+        set("buildToolsVersion", "35.0.0")
+        set("minSdkVersion", 24)
+        set("compileSdkVersion", 35)
+        set("targetSdkVersion",34)
+        set("ndkVersion", "26.1.10909125")
+        set("kotlinVersion", "1.9.24")
+    }
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle")
+        classpath("com.facebook.react:react-native-gradle-plugin")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin")
+    }
+}
+
