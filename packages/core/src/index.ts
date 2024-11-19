ecc93911b894f1d81e94b784372206ea2ce4e79f
Damian Mussi <mutsi@Damians-MBP.fibertel.com.ar>
2024-11-19 16:16:07 -0300

    add Try Catch

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -125,16 +125,22 @@ const initialize = async ({
   // Only attempt to check for CodePush bundle URL in release mode. Otherwise CodePush will throw an exception.
   // https://docs.microsoft.com/en-us/appcenter/distribution/codepush/react-native#plugin-configuration-ios
   if (!__DEV__) {
-    const isCodePushPresent =
-      await NativeModules.EmbraceManager.checkAndSetCodePushBundleURL();
+    try {
+      const isCodePushPresent =
+        await NativeModules.EmbraceManager.checkAndSetCodePushBundleURL();
 
-    if (!isCodePushPresent && Platform.OS === "ios") {
-      const bundleJs =
-        await NativeModules.EmbraceManager.getDefaultJavaScriptBundlePath();
+      if (!isCodePushPresent && Platform.OS === "ios") {
+        const bundleJs =
+          await NativeModules.EmbraceManager.getDefaultJavaScriptBundlePath();
 
-      if (bundleJs) {
-        await NativeModules.EmbraceManager.setJavaScriptBundlePath(bundleJs);
+        if (bundleJs) {
+          NativeModules.EmbraceManager.setJavaScriptBundlePath(bundleJs);
+        }
       }
+    } catch (e) {
+      console.warn(
+        "[Embrace] We were unable to set the JSBundle path automatically. Please configure this manually to enable crash symbolication. For more information see https://embrace.io/docs/react-native/integration/upload-symbol-files/#pointing-the-embrace-sdk-to-the-javascript-bundle.",
+      );
     }
   }
 
