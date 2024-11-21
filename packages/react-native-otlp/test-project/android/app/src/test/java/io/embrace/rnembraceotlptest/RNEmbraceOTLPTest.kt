3535546fd1ae63d80014e9488f3fa4902a5b6ad1
Florencia Acosta <florencia.acosta@embrace.io>
2024-11-21 11:34:26 -0300

    adding test case for running workflow

--- packages/react-native-otlp/test-project/android/app/src/test/java/io/embrace/rnembraceotlptest/RNEmbraceOTLPTest.kt
+++ packages/react-native-otlp/test-project/android/app/src/test/java/io/embrace/rnembraceotlptest/RNEmbraceOTLPTest.kt
@@ -9,6 +9,7 @@ import com.facebook.react.bridge.WritableMap
 import io.embrace.rnembraceotlp.RNEmbraceOTLPModule
 import org.junit.Before
 import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.runner.RunWith
 import org.mockito.Mockito.mockStatic
 import org.mockito.Mockito.`when`
@@ -16,16 +17,39 @@ import org.mockito.kotlin.mock
 import org.mockito.kotlin.times
 import org.mockito.kotlin.verify
 import org.robolectric.RobolectricTestRunner
+import java.util.logging.Handler
+import java.util.logging.LogRecord
+import java.util.logging.Logger
+
+// Custom handler to capture log records
+private class LogEmbraceHandler : Handler() {
+    val records = mutableListOf<LogRecord>()
+
+    override fun publish(record: LogRecord) {
+        records.add(record)
+    }
+
+    override fun flush() {}
+    override fun close() {}
+}
 
 @RunWith(RobolectricTestRunner::class)
 class RNEmbraceOTLPTest {
     private val promise: Promise = mock()
+    private lateinit var logHandler: LogEmbraceHandler
 
     @Before
     fun setUp() {
         mockStatic(SystemClock::class.java).use {
             `when`(SystemClock.uptimeMillis()).thenReturn(1000L)
         }
+
+        // Intercepting logs
+        val log = Logger.getLogger("[Embrace]")
+
+        logHandler = LogEmbraceHandler()
+        log.addHandler(logHandler)
+        log.useParentHandlers = false
     }
 
     @Test
@@ -81,6 +105,10 @@ class RNEmbraceOTLPTest {
         val embraceOTLPModule = RNEmbraceOTLPModule(context)
 
         embraceOTLPModule.startNativeEmbraceSDK(JavaOnlyMap(), JavaOnlyMap(), promise)
+
+        val logs = logHandler.records.map { it.message }
+        assertEquals(listOf("Neither Traces nor Logs configuration were found, skipping custom export."), logs)
+        println(logs)
 
         // embrace starts without issues if empty object is passed down to the export configuration
         verify(promise, times(1)).resolve(true)
@@ -147,6 +175,42 @@ class RNEmbraceOTLPTest {
         otlpConfig.putMap("logExporter", logExporter)
 
         embraceOTLPModule.startNativeEmbraceSDK(JavaOnlyMap(), otlpConfig, promise)
+
+        // embrace starts without issues if only log export config is found
+        verify(promise, times(1)).resolve(true)
+    }
+
+    @Test
+    fun testInvalidHeader() {
+        val context: ReactApplicationContext = mock()
+        val embraceOTLPModule = RNEmbraceOTLPModule(context)
+        val otlpConfig: WritableMap = JavaOnlyMap()
+
+        val logExporter: WritableMap = JavaOnlyMap().apply {
+            putString("endpoint", "https://test-log-endpoint/v1")
+            putArray(
+                "headers",
+                JavaOnlyArray().apply {
+                    pushMap(
+                        JavaOnlyMap().apply {
+                            putString("key", null)
+                            putString("token", "")
+
+                            putString("key2", "key_for_header")
+                            putString("token2", "token_for_header")
+                        }
+                    )
+                }
+            )
+            putDouble("timeout", 5000.0)
+        }
+
+        otlpConfig.putMap("logExporter", logExporter)
+
+        embraceOTLPModule.startNativeEmbraceSDK(JavaOnlyMap(), otlpConfig, promise)
+        val logs = logHandler.records.map { it.message }
+        print(logs)
+        assertEquals(listOf("Skipping invalid header. `key` and/or `token` are null or blank."), logs)
 
         // embrace starts without issues if only log export config is found
         verify(promise, times(1)).resolve(true)
