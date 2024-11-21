9603f137e5c591a1f172866a16b5151810c996c6
Florencia Acosta <florencia.acosta@embrace.io>
2024-11-21 14:58:16 -0300

    fixing tests

--- scripts/run-ios-tests.sh
+++ scripts/run-ios-tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 WORKSPACE=$1
 SCHEME=$2
-xcodebuild test -workspace "$WORKSPACE" -scheme "$SCHEME" -sdk "$IOS_TEST_SDK" -enableCodeCoverage YES -destination "platform=$IOS_TEST_PLATFORM,name=$IOS_TEST_DEVICE,OS=$IOS_TEST_OS" -configuration Debug | xcbeautify
+xcodebuild test -workspace "$WORKSPACE" -scheme "$SCHEME" -sdk "$IOS_TEST_SDK" -enableCodeCoverage YES -destination "platform=$IOS_TEST_PLATFORM,name=$IOS_TEST_DEVICE,OS=$IOS_TEST_OS" | xcbeautify
 exit  "${PIPESTATUS[0]}"
