2662f7e0d22fac8ae4f0b04956323ff0e8c7fc4e
Jonathan Munz <jpmunz@gmail.com>
2025-01-13 16:23:33 -0500

    add tracerProvider to dependencies list

--- packages/react-native-tracer-provider/src/useEmbraceNativeTracerProvider.ts
+++ packages/react-native-tracer-provider/src/useEmbraceNativeTracerProvider.ts
@@ -71,8 +71,7 @@ export const useEmbraceNativeTracerProvider = (
         })
         .finally(() => setIsLoading(false));
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isLoading, config, enabled]);
+  }, [isLoading, config, enabled, tracerProvider]);
 
   return {
     isLoading,
