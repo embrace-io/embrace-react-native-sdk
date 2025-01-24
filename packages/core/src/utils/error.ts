08674227f830358a258b58347f312b529947cf13
Jonathan Munz <jpmunz@gmail.com>
2025-01-24 14:27:33 -0500

    EMBR-6322 update const naming

--- packages/core/src/utils/error.ts
+++ packages/core/src/utils/error.ts
@@ -20,7 +20,7 @@ const trackUnhandledRejection = (_: unknown, error: Error) => {
   );
 };
 
-const rejectionTrackingConfig = {
+const REJECTION_TRACKING_CONFIG = {
   allRejections: true,
   onUnhandled: trackUnhandledRejection,
   onHandled: () => {},
@@ -33,12 +33,12 @@ const enableUnhandledRejectionTracking = () => {
   // Do the same checking as react-native to make sure we add tracking to the right Promise implementation
   // https://github.com/facebook/react-native/blob/v0.77.0/packages/react-native/Libraries/Core/polyfillPromise.js#L25
   if (hermesInternal?.hasPromise?.()) {
-    hermesInternal?.enablePromiseRejectionTracker?.(rejectionTrackingConfig);
+    hermesInternal?.enablePromiseRejectionTracker?.(REJECTION_TRACKING_CONFIG);
   } else {
     // [Unhandled Rejections](https://github.com/then/promise/blob/master/Readme.md#unhandled-rejections)
     // [promise/setimmediate/rejection-tracking](https://github.com/then/promise/blob/master/src/rejection-tracking.js)
     require("promise/setimmediate/rejection-tracking").enable(
-      rejectionTrackingConfig,
+      REJECTION_TRACKING_CONFIG,
     );
   }
 };
