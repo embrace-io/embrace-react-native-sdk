342841c91eb4a18a8abf4b0435cde44116204924
Jonathan Munz <jpmunz@gmail.com>
2025-03-07 10:25:10 -0500

    EMBR-6574 upgrade native SDKs (#419)

--- yarn.config.cjs
+++ yarn.config.cjs
@@ -127,8 +127,8 @@ function enforceEmbraceMetadata({Yarn}) {
     }
 
     workspace.set("embrace", {
-      iosVersion: "6.7.1",
-      androidVersion: "7.0.0",
+      iosVersion: "6.8.1",
+      androidVersion: "7.2.0",
     });
   }
 }
