498fc8565084fa2b16998c3280155aff0eadc5b1
Florencia Acosta <florencia.acosta@embrace.io>
2025-07-29 17:30:19 -0300

    [EMBR-6298] Add test template for RN 0.80.x and Expo RN 0.79.x

---
+++ integration-tests/templates/rn80-template/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {EmbraceTestHarness} from '@embrace-io/react-native-test-harness';
+import sdkConfig from './embrace-sdk-config.json';
+
+const App = () => {
+  return (
+    <EmbraceTestHarness
+      navigationStyle="react-native"
+      sdkConfig={sdkConfig}
+      allowCustomExport={false}
+    />
+  );
+}
+
+export default App;
