30d6f501c8a4b265aa75cfc4d92b6c27a9a004c0
Florencia Acosta <florencia.acosta@embrace.io>
2025-08-18 12:39:44 -0300

    6.3.0 Release

--- yarn.config.cjs
+++ yarn.config.cjs
@@ -128,7 +128,7 @@ function enforceEmbraceMetadata({Yarn}) {
 
     workspace.set("embrace", {
       iosVersion: "6.8.5",
-      androidVersion: "7.6.1",
+      androidVersion: "7.8.0",
     });
   }
 }
