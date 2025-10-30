313215173bb20fa4818fa314051400b2b6757572
alyssa-embrace <alyssa.syharath@embrace.io>
2025-10-30 14:47:05 -0400

    Trying out different syntax

--- scripts/run-ios-tests.sh
+++ scripts/run-ios-tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 WORKSPACE=$1
 SCHEME=$2
-xcodebuild test -workspace "$WORKSPACE" -scheme "$SCHEME" -sdk "$IOS_TEST_SDK" -enableCodeCoverage YES -destination "platform=$IOS_TEST_PLATFORM,name=$IOS_TEST_DEVICE,OS=$IOS_TEST_OS" -parallel-testing-enabled NO -maximum-parallel-testing-workers 1 -jobs 1 -parallelizeTargets NO | xcbeautify
+xcodebuild test -workspace "$WORKSPACE" -scheme "$SCHEME" -sdk "$IOS_TEST_SDK" -enableCodeCoverage YES -destination "platform=$IOS_TEST_PLATFORM,name=$IOS_TEST_DEVICE,OS=$IOS_TEST_OS" -disable-concurrent-testing -jobs 1 | xcbeautify
 exit  "${PIPESTATUS[0]}"
