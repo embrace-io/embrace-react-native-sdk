985654977a34ce17759439d53a9a7627b07b3c6f
alyssa-embrace <alyssa.syharath@embrace.io>
2025-10-30 03:19:33 -0400

    shell push

--- scripts/run-ios-tests.sh
+++ scripts/run-ios-tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 WORKSPACE=$1
 SCHEME=$2
-xcodebuild test -workspace "$WORKSPACE" -scheme "$SCHEME" -sdk "$IOS_TEST_SDK" -enableCodeCoverage YES -destination "platform=$IOS_TEST_PLATFORM,name=$IOS_TEST_DEVICE,OS=$IOS_TEST_OS" | xcbeautify
+xcodebuild test -workspace "$WORKSPACE" -scheme "$SCHEME" -sdk "$IOS_TEST_SDK" -enableCodeCoverage YES -destination "platform=$IOS_TEST_PLATFORM,name=$IOS_TEST_DEVICE,OS=$IOS_TEST_OS" -parallel-testing-enabled NO -maximum-parallel-testing-workers 1 -jobs 1 -parallelizeTargets NO | xcbeautify
 exit  "${PIPESTATUS[0]}"
