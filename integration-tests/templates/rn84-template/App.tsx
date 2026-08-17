e579183b1aa67b3441a0ad20d35467fa0bd38a3e
Ahmed Ahmed <74918474+hamudi-ahmed@users.noreply.github.com>
2026-08-17 17:47:09 +0100

    [EMBR-13645] Update test templates and reduce CI test matrix (#990)

---
+++ integration-tests/templates/rn84-template/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {EmbraceReactNativeTestHarness} from '@embrace-io/react-native-test-harness';
+import sdkConfig from './embrace-sdk-config.json';
+
+function App(): React.JSX.Element {
+  return (
+    <EmbraceReactNativeTestHarness
+      sdkConfig={sdkConfig}
+      allowCustomExport={false}
+    />
+  );
+}
+
+export default App;
